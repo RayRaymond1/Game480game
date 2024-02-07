c95e6ea30573ea76504b4ad170476ddfa3d8bdf3
RayRaymond1 <beefbro11@gmail.com>
2024-02-07 03:22:58 -0500

    selectively added textures to shrine, added new assets

---
+++ Game 480/Assets/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //setting these to public so we can mess around with them
+    public float health = 100f;
+    public 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
