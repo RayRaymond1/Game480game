204e525328348b8eed2c665840ba5c97d8bb52fb
Jeremy Myers <ImInUrCheese54@gmail.com>
2024-03-29 17:07:58 -0400

    Enemy bug fix and level 2 enemy prototype

---
+++ Game 480/Assets/Chracters/Level 1 Enemy/EnemyAI.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class EnemyAI : MonoBehaviour
+{
+    private EnemyReferences enemyReferences;
+    private float pathUpdateDeadline;
+    private float stopDistance;
+    private Vector3 startPosition;
+    public bool reseting = false;
+    private bool newWord = false;
+    public bool inRange = false;
+    public float speedScalar = 6f;
+    
+    void Awake(){
+        enemyReferences = GetComponent<EnemyReferences>(); 
+        startPosition = transform.position;
+    }
+    void Start(){
+        stopDistance = enemyReferences.agent.stoppingDistance;
+        enemyReferences.eventManager.wordCompleteEvent.AddListener(CompleteWord);
+    }
+    void Update()
+    {
+    if(enemyReferences.target != null){
+        inRange = Vector3.Distance(transform.position, enemyReferences.target.position) <= stopDistance + .42f;
+        Debug.Log("Distance: " + Vector3.Distance(transform.position, startPosition));
+        if(reseting){
+            UpdatePath(startPosition);
+            Debug.Log("Resetting");
+        } else{
+            UpdatePath(enemyReferences.target.position);
+            if(newWord){
+                newWord = false;
+                enemyReferences.eventManager.ResetWord.Invoke();
+            }
+        }
+        if(Vector3.Distance(transform.position, startPosition) <= 0.22f){
+            reseting = false;
+        }
+        LookAtTarget();
+        enemyReferences.animator.SetBool("Attacking", inRange);
+        enemyReferences.animator.SetBool("Reseting", reseting);
+        if(enemyReferences.typer.currentWord.Length > 0 && !reseting){
+            setSpeed(1f/enemyReferences.typer.currentWord.Length * speedScalar);
+        }else if (reseting){
+            setSpeed(7);
+        }else {
+            setSpeed(1);
+        }
+    }
+}
+    void LookAtTarget(){
+        Vector3 lookPos = (enemyReferences.target.position - transform.position);
+        lookPos.y = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 0.2f);
+    }
+    void UpdatePath(Vector3 targetPosition){
+        if(reseting){
+            enemyReferences.agent.stoppingDistance = 0;
+        }else{
+            enemyReferences.agent.stoppingDistance = stopDistance;
+        }
+        if(Time.time >= pathUpdateDeadline){
+            pathUpdateDeadline = Time.time + enemyReferences.pathUpdateDelay;
+            enemyReferences.agent.SetDestination(targetPosition);
+        }
+    }
+    void AttackEnd(){
+        enemyReferences.animator.SetBool("Attacking", false);
+        reseting = true;
+    }
+    void AttackStart(){
+        enemyReferences.eventManager.wordFailedEvent.Invoke();
+    }
+    void CompleteWord(){
+        reseting = true;
+        newWord = true;
+    }
+    void setSpeed(float speed){
+        enemyReferences.agent.speed = speed;
+    }
+}
---
+++ Game 480/Assets/Chracters/Level 1 Enemy/WordBank.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordBank : MonoBehaviour
+{
+    public List<string> originalWords = new List<string> 
+    { 
+        "Honor", "Strength", "Skill", "Clarity", "Loyalty", "Mastery", "Bushido"
+    };
+    private List<string> currentWords = new List<string>();
+
+    void Awake()
+    {
+        currentWords.AddRange(originalWords);
+        Shuffle(currentWords);
+        ConvertToLower(currentWords);
+    }
+
+    void Shuffle(List<string> words)
+    {
+        for (int i = 0; i < words.Count; i++)
+        {
+            string temp = words[i];
+            int randomIndex = Random.Range(i, words.Count);
+            words[i] = words[randomIndex];
+            words[randomIndex] = temp;
+        }
+    }
+
+    void ConvertToLower(List<string> words)
+    {
+        for (int i = 0; i < words.Count; i++)
+        {
+            words[i] = words[i].ToLower();
+        }
+    }
+
+    public string GetWord()
+    {
+        string newWord = string.Empty;
+        if(currentWords.Count != 0)
+        {
+            newWord = currentWords.Last();
+            currentWords.Remove(newWord);
+        }
+        return newWord;
+    }
+}
