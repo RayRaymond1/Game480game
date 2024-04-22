11d7cdc9a54843c4f63d17e2e19d61942997264c
Lattori <106184112+Lattori@users.noreply.github.com>
2024-04-22 14:25:30 -0400

    yellow healthbar and dialogue text speed increase

--- Game 480/Assets/Dialogue.cs
+++ Game 480/Assets/Dialogue.cs
@@ -5,57 +5,62 @@ using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
-    // Start is called before the first frame update
     public TextMeshProUGUI textComponent;
     public string[] lines;
     public float textSpeed;
+
     private int index;
+
+    // Start is called before the first frame update
     void Start()
     {
         textComponent.text = string.Empty;
         StartDialogue();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             if (textComponent.text == lines[index])
             {
                 NextLine();
             }
-            else { 
+            else
+            {
                 StopAllCoroutines();
                 textComponent.text = lines[index];
             }
         }
-        
     }
 
-    void StartDialogue() {
+    void StartDialogue()
+    {
         index = 0;
         StartCoroutine(TypeLine());
-    
     }
 
-    IEnumerator TypeLine() {
-        foreach (char c in lines[index].ToCharArray()) {
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
-        
         }
     }
-    void NextLine() {
 
+    void NextLine()
+    {
         if (index < lines.Length - 1)
         {
             index++;
-
             textComponent.text = string.Empty;
             StartCoroutine(TypeLine());
         }
-        else {
+        else
+        {
             gameObject.SetActive(false);
         }
     }
